16214f5a2abf1b614f87ead4606f8d0291a75999
highflyer910 <thea.mushambadze@gmail.com>
2022-11-03 04:01:14 +0400

    update

--- src/consts.ts
+++ src/consts.ts
@@ -11,21 +11,21 @@ export const ROUTES = [
 
 export const SOCIAL = [
   {
-    href: 'https://twitter.com/EmaSuriano',
+    href: 'https://twitter.com/highflyer910',
     icon: 'twitter',
   },
   {
-    href: 'https://www.linkedin.com/in/emanuel-suriano/',
+    href: 'https://www.linkedin.com/in/tmushambadze/',
     icon: 'linkedIn',
   },
 ] as const;
 
 export const PERSONAL_INFO = {
-  name: 'Ema Suriano',
-  title: "Ema Suriano's Art",
-  subtitle: 'A collection of my best work in one place',
+  name: 'Teona',
+  title: "In-Game Photography",
+  subtitle: 'A collection of photos taken in VR',
   role: 'Digital Artist',
-  contact: 'mailto:emanuel.suriano@gmail.com',
+  contact: 'mailto:mushambadzeteona@gmail.com',
   avatar:
     'https://cdn.discordapp.com/attachments/1023544104698773564/1025325255843127386/Ema_portrait_of_a_young_men_with_snowy_mountains_at_the_backgro_9c3a800b-294c-481b-8c57-0c84ba46ba08.png',
   about:
