7207e547cd7e7bebd71de0c273125acd6b4fed03
Thea Mushambadze <thea.mushambadze@gmail.com>
2023-04-03 21:42:12 +0400

    update

--- src/consts.ts
+++ src/consts.ts
@@ -8,11 +8,11 @@ export const SOCIAL = [
 export const PERSONAL_INFO = {
   name: 'Teona',
   title: "In-Game Photography",
-  subtitle: 'A collection of photos taken in VR',
+  subtitle: 'Through the Lens of Virtual Worlds: A Humble Collection of VR Photography',
   role: 'In-Game Photographer',
   contact: 'mailto:mushambadzeteona@gmail.com',
   avatar:
     'https://res.cloudinary.com/highflyer910/image/upload/v1668194744/ingame_pics/5.png',
   about:
-    'I am a Frontend Engineer, but I enjoy creating art. My creations are made during journeys in virtual worlds, and later on improved using different graphic tools.',
+    "I am a Frontend Engineer, but beyond the digital realm, I find joy in creating art as well. My artistic creations take shape during explorations of virtual worlds, where I use my technical prowess to bring my visions to life. Afterwards, I further refine my works with a variety of graphic tools, pushing the limits of my creativity and skillset. Through my photography, I seek to capture moments of beauty and emotion, both in the virtual and physical worlds, and share them with others.",
 } as const;
