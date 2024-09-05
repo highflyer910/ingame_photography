f2881b4d17a1ff655ebe2e64eb07cab3cbd07a96
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-09-06 02:00:15 +0400

    update

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import { astroImageTools } from 'astro-imagetools';
-
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [astroImageTools, tailwind()],
-});
